be970378d191bb9e86e37a987d176f77de4bf8ee
Z0UH4IRI <mohamedzouhairi023@gmail.com>
2025-02-19 12:26:21 +0100

    Role Check Middleware Done

--- app/Http/Middleware/CheckRole.php
+++ app/Http/Middleware/CheckRole.php
@@ -17,7 +17,7 @@ class CheckRole
     public function handle(Request $request, Closure $next, $role): Response
     {
 
-       if(Auth::check() || Auth::user()->role!=$role)
+       if(!Auth::check() || Auth::user()->role!==$role)
        {
         abort(403, 'Unauthorized Access');
        }
--- routes/web.php
+++ routes/web.php
@@ -20,12 +20,37 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth', 'verified', 'role:user'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware('auth', 'role:user')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+
+// middleware for agent routes
+
+Route::middleware(['auth', 'role:agent'])->group(function () {
+
+    Route::get('/agent/dashboard', function () {
+        return view('welcome agent');
+    });
+
+
+});
+
+
+// middleware for admin routes
+
+Route::middleware(['auth', 'role:admin'])->group(function () {
+
+    Route::get('/admin/dashboard', function () {
+        echo 'welcome admin';
+    });
+
+
+});
+
 
 require __DIR__.'/auth.php';
